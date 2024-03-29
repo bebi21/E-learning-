539232f9a4652e5316805166731bc09cecd5529a
bebi21 <nguyenlocnd18@gmail.com>
2024-03-29 14:40:45 +0700

    add user , subscription

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
+import { SubscriptionModule } from './subscription/subscription.module';
+import { SubscriptionController } from './subscription/subscription.controller';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { User } from './user/entity/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SubscriptionModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', // type of database
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'loc214',
+      database: 'project5',
+
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+
+  controllers: [SubscriptionController],
+  providers: [],
 })
 export class AppModule {}
