9cd8ab052fa4f1dd88e683c34cba743cb21e220b
bebi21 <nguyenlocnd18@gmail.com>
2024-04-01 08:18:36 +0700

    config all  can run

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
+import { SubscriptionModule } from './subscription/subscription.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 import { User } from './user/entity/user.entity';
-import { CourseModule } from './course/course.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './course/entities/course.entity';
 import { Subscription } from './subscription/entity/subscription.entity';
-import { SubscriptionModule } from './subscription/subscription.module';
-import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
+
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
+
 import { CloudinaryService } from './cloudinary/cloudinary.service';
 import { CloudinaryController } from './cloudinary/cloudinary.controller';
 
@@ -20,7 +22,7 @@ import { CloudinaryController } from './cloudinary/cloudinary.controller';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: '',
+      password: 'loc214',
       database: 'e-learning',
       entities: [Course, User, Subscription],
       synchronize: true,
@@ -30,6 +32,5 @@ import { CloudinaryController } from './cloudinary/cloudinary.controller';
   ],
   controllers: [CloudinaryController],
   providers: [CloudinaryService],
-
 })
 export class AppModule {}
